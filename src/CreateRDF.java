89ff97896aeca9258206ad696108c611ec7846d9
Imen Frigui <imen_frigui@outlook.com>
2024-10-14 14:31:12 +0100

    feat: creating antology model; connect to base Url

--- src/CreateRDF.java
+++ src/CreateRDF.java
@@ -1,5 +1,16 @@
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.OWL;
+
 public class CreateRDF {
     public void create(){
+        String baseURI = "http://www.semanticweb.org/imenfrigui/ontologies/2024/8/PlanificateurTrajetsEcologiques#";
+        Model model = ModelFactory.createDefaultModel();
 
+        Resource ontology = model.createResource("http://www.semanticweb.org/imenfrigui/ontologies/2024/8/PlanificateurTrajetsEcologiques")
+                .addProperty(OWL.sameAs, baseURI);
+
+        model.write(System.out, "RDF/XML");
     }
 }
