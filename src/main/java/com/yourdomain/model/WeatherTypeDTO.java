3630a0537d971a827a1759ae23e1ad5e9cf6eb76
Imen Frigui <imen_frigui@outlook.com>
2024-10-27 12:18:55 +0100

    WeatherController get and add

---
+++ src/main/java/com/yourdomain/model/WeatherTypeDTO.java
@@ -0,0 +1,15 @@
+package com.yourdomain.model;
+
+public class WeatherTypeDTO {
+    private String weatherType;
+
+    // Getter
+    public String getWeatherType() {
+        return weatherType;
+    }
+
+    // Setter
+    public void setWeatherType(String weatherType) {
+        this.weatherType = weatherType;
+    }
+}
